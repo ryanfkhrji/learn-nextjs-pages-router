0a1b49277f6e71001cae6c8ac88eb3c34cfde90b
ryanfkhrji <ryanfakhroji09@gmail.com>
2025-07-27 21:32:36 +0700

    Change Url Api

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -32,7 +32,7 @@ export async function getServerSideProps({params}: {params: {product: string}}) 
   // params.product itu adalah slug yang kita buat di [product].tsx
 
   // fecth data
-  const res = await fetch(`http://localhost:3000/api/product/${params.product}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/product/${params.product}`);
   const response = await res.json();
 
   return {
--- src/pages/product/server.tsx
+++ src/pages/product/server.tsx
@@ -17,7 +17,7 @@ export default ProductPage;
 // dipanggil setiap melakukan request
 export async function getServerSideProps() {
   // fecth data
-  const res = await fetch(`http://localhost:3000/api/product`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/product`);
   const response = await res.json();
 
   return {
